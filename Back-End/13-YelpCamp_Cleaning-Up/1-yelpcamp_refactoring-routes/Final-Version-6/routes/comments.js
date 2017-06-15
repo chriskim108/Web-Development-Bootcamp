5481d5514e894732f1b08b42560bad7461beb3cb
Christopher Kim <chriskim108@gmail.com>
2017-06-15 14:17:24 -0400

    Add files via upload

---
+++ Back-End/13-YelpCamp_Cleaning-Up/1-yelpcamp_refactoring-routes/Final-Version-6/routes/comments.js
@@ -0,0 +1,51 @@
+//We are going to use the express router, instead of redeclaring each variables
+var express = require("express");
+//This will merge the parameter froms the campgrounds and the comments together
+var router = express.Router({mergeParams: true});
+var Campground = require("../models/campground");
+var Comment = require("../models/comment");
+// ====================
+// COMMENTS ROUTES
+// ====================
+//Comments New
+router.get("/new", isLoggedIn, function(req, res){
+    // find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+             res.render("comments/new", {campground: campground});
+        }
+    })
+});
+
+//Comments Create
+router.post("/",isLoggedIn,function(req, res){
+   //lookup campground using ID
+   Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+        Comment.create(req.body.comment, function(err, comment){
+           if(err){
+               console.log(err);
+           } else {
+               campground.comments.push(comment);
+               campground.save();
+               res.redirect('/campgrounds/' + campground._id);
+           }
+        });
+       }
+   });
+});
+
+//Middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
